16c4c41f6712b355555a05170d3abeb447fc40a3
riraq <riraq@mail.usf.edu>
2021-01-24 13:34:26 -0600

    added dynamic date to header

---
+++ script.js
@@ -0,0 +1,10 @@
+// sets the time in the header of the page
+var now = moment().format("dddd, MMMM Do");
+console.log(now)
+$("#currentDay").text(now);
+
+// -------------------------------------------------------------------
+$(document).ready(function(){
+    
+
+})
