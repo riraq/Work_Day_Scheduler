2f3171b6e28be8202852e7cd737adaff3cbde319
riraq <riraq@mail.usf.edu>
2021-01-24 16:07:07 -0600

    added pseudo code in js file, updated bootstrap version link

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
 // sets the time in the header of the page
 var now = moment().format("dddd, MMMM Do");
-console.log(now)
 $("#currentDay").text(now);
 
 // -------------------------------------------------------------------
 $(document).ready(function(){
+    // variables to use
+    // .description
+    // .time-block
+    // .row
+    // .hour
+    // .past
+    // .present
+    // .future
+    // .saveBtn
+    // .saveBtn i:hover
     
+    // create timeblocks
+    // make a for loop that iterates elements to build the timeblock structure
+    
+    // add times to blocks
+    // use for loop to create column elements for time (or does jQuery automatically append to everything with the selected class/id?)
+    // use for loop to iterate times into the time column
+
+    // add text area for blocks
+    // use for loop to create column elements for work tasks
+    
+    // add save button for each timeblock
+    // add on click for the button 
+    // when button clicked, save to local storage
+
+    // use for loop/check jQuery to append icon and 
+
+    // change color of timeblock based on time of day
+    // if/else statement
+
+
 
 })
