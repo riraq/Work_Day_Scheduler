b8c12528e05a1e6f1dad0c1c1db73fab0574daa2
riraq <riraq@mail.usf.edu>
2021-01-25 14:58:59 -0600

    added schedule structure to html to be static, rather than dynamic through jquery

--- script.js
+++ script.js
@@ -5,43 +5,11 @@ $("#currentDay").text(now);
 
 // -------------------------------------------------------------------
 $(document).ready(function(){
-    // variables to use
-    // .description
-    // textarea
-    // .time-block
-    // .row
-    // .hour
-    // .past
-    // .present
-    // .future
-    // .saveBtn
-    // .saveBtn i:hover
 
-    var schedulerContainer = $(".container");
+    var container = $(".container");
     var workHours = ["9am","10am","11am","12pm","1pm","2pm","3pm","4pm","5pm"];
     var militaryHours = [9,10,11,12,13,14,15,16,17];
-
-    // create timeblocks
-    // make a for loop that iterates elements to build the timeblock structure
-    for (var i=0; i<workHours.length; i++) {
-        var timeblockRows = $("<div>");
-        timeblockRows.addClass("row");
-        schedulerContainer.append(timeblockRows);
-    };
-
-    // separate the rows into columns by selecting rows, appending 3 new elements to each row, adding bootstrap column classes to newly appended elements
-    $(".row").append("<span class= 'col hour'>")
-    $(".row").append("<textarea class= 'col-8'>")
-    $(".row").append("<button class= 'col saveBtn'>")
-
-    // add times to blocks
-    // use for loop to create column elements for time (or does jQuery automatically append to everything with the selected class/id?)
-    // use for loop to iterate times into the time column
-
-    // add text area for blocks
-    // use for loop to create column elements for work tasks
     
-    // add save button for each timeblock
     // add on click for the button 
     // when button clicked, save to local storage
 
