511942c324745857694bd3b1bd868ce33e48ff5c
riraq <riraq@mail.usf.edu>
2021-01-26 18:37:00 -0600

    classes being added to html with jquery, added $.each loop for changing color of row based on time of day

--- script.js
+++ script.js
@@ -1,23 +1,45 @@
 // sets the time in the header of the page
 var now = moment().format("dddd, MMMM Do");
-var nowTime = moment().format("dddd, MMMM Do YYYY, h:mm:ss a")
+var nowTime = moment().format("H")
 $("#currentDay").text(now);
+console.log(nowTime)
 
 // -------------------------------------------------------------------
 $(document).ready(function(){
+    var rowEl = $(".row")
+    var spanEl = $("span")
+    var textAreaEl = $("textarea")
+    var buttonEl = $("button")
 
-    var container = $(".container");
-    var workHours = ["9am","10am","11am","12pm","1pm","2pm","3pm","4pm","5pm"];
-    var militaryHours = [9,10,11,12,13,14,15,16,17];
-    
-    // add on click for the button 
-    // when button clicked, save to local storage
-
-    // use for loop/check jQuery to append icon and 
+    spanEl.addClass("col hour")
+    textAreaEl.addClass("col-8")
+    buttonEl.addClass("col saveBtn fas fa-save")
 
     // change color of timeblock based on time of day
     // if/else statement
+    console.log($("textarea")[0])
+    console.log(parseInt(rowEl[5].dataset.value))
+
+    rowEl.each(function(){
+        
+        var rowHour = parseInt(this.dataset.value)
+        
+        if (rowHour < nowTime){
+        $(this).children("textarea").addClass("past")
+        }
+        else if (rowHour == nowTime) {
+        $(this).children("textarea").addClass("present")
+        }
+        else {
+        $(this).children("textarea").addClass("future")
+        }
+    })
 
 
+    // add on click for the button 
+    // when button clicked, save to local storage
+    // when page loads, look at local storage for each row to load
 
 })
+
+
